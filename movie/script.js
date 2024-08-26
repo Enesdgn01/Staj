cb336c1db569a4d2e41113bb517d2a55aa9680a6
Şefik Enes Doğan <147730683+Enesdgn01@users.noreply.github.com>
2024-08-26 15:44:25 +0300

    config

--- movie/script.js
+++ movie/script.js
@@ -2,7 +2,7 @@ const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+  'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=';
 
 window.onload = (event) => {
   getAPI();
@@ -19,6 +19,7 @@ async function getAPI() {
     .then(function (data) {
       let output = "";
       data.results.forEach(function (item) {
+        const voteAverage = item.vote_average.toFixed(1); 
         output += `
         <div class="m-container">
           <div class="m-image">
@@ -26,7 +27,7 @@ async function getAPI() {
           </div>
           <div class="m-content">
             <div class="m-name">${item.title}</div>
-            <div class="m-imdb"> ${item.vote_average}</div>
+            <div class="m-imdb">${voteAverage}</div>
           </div>
           <div class="overview">
             <h3>Overview</h3>
@@ -34,17 +35,19 @@ async function getAPI() {
           </div>
         </div>`;
       });
-      document.querySelector(".main").innerHTML = output;
+      main.innerHTML = output;
     });
 }
+
 async function getSearch(keyword) {
-  fetch(SEARCH_API+keyword)
+  fetch(SEARCH_API +(keyword)) 
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       let output = "";
       data.results.forEach(function (item) {
+        const voteAverage = item.vote_average.toFixed(1); 
         output += `
         <div class="m-container">
           <div class="m-image">
@@ -52,7 +55,7 @@ async function getSearch(keyword) {
           </div>
           <div class="m-content">
             <div class="m-name">${item.title}</div>
-            <div class="m-imdb"> ${item.vote_average}</div>
+            <div class="m-imdb">${voteAverage}</div>
           </div>
           <div class="overview">
             <h3>Overview</h3>
@@ -60,13 +63,13 @@ async function getSearch(keyword) {
           </div>
         </div>`;
       });
-      document.querySelector(".main").innerHTML = output;
+      main.innerHTML = output;
     });
 }
+
 document.addEventListener("keydown", function (e) {
-    if (e.code === "Enter") {  
-        const keyword = document.querySelector(".search").value
-        getSearch(keyword);
-    }
+  if (e.code === "Enter") {
+    const keyword = search.value;
+    getSearch(keyword);
+  }
 });
-  
